8edcf7ec9eb3370852169eae55b2b56219b46d58
Mac <huzaifasadiq125@gmail.com>
2022-01-14 15:26:35 +0100

    Add Edit Api for Nest and Nest Item
    adding and editing nest and nest item

--- backend/nest.js
+++ backend/nest.js
@@ -2,11 +2,79 @@ const router = require('express').Router();
 const sqlManager = require('./sql');
 const config = require('./config');
 const jwt = require('jsonwebtoken');
+const multer  = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  });
+const upload = multer({storage: storage});
+
+
+//Add new Nest
+router.post('/addNest', upload.array('image',1), async function(req, res) {
+    var photo = "https://www.froben11.de/wp-content/uploads/2016/10/orionthemes-placeholder-image.png";
+    if(req.files && req.files.length > 0) {
+        photo = req.files[0].path;
+    }
+    nest = {
+        name: req.body.name,
+        description: req.body.description,
+        user_id: req.body.user_id,
+        photo: photo
+    }
+
+    sqlManager.addNest(nest, function(err, result) {
+        if (err) {
+            res.status(500).json({status:'Failed', message: err.message});
+            return
+        }
+        res.status(200).json({status:'Success', message:'Nest Added Succesfully'});
+    });
+})
+
+router.put('/editNest', upload.array('image',1), async function(req, res) {
+    
+    nest = {
+        id : req.body.id,
+        name: req.body.name,
+        description: req.body.description,
+        favored: req.body.favored ?? 0
+    }
+
+    if(req.files && req.files.length > 0) {
+        nest.photo = req.files[0].path;
+    }
+
+    sqlManager.editNest(nest, function(err, result) {
+        if (err) {
+            res.status(500).json({status:'Failed', message: err.message});
+            return
+        }
+        res.status(200).json({status:'Success', message:'Nest Edited Succesfully'});
+    });
+})
+
+//delete Nest
+router.delete('/deleteNest', async function(req, res) {
+    let nestID = req.body.nest_id;
+    console.log(nestID);
+    sqlManager.deleteNest(nestID, async function(err, result) {
+        if (err) {
+            res.status(500).json({status:'Failed', message: err.message});
+            return
+        }
+        res.status(200).json({result: result});    
+    });
+})
 
 
 //get Nests for user
 router.get('/userNests', async function(req, res) {
-    let userID = req.params.user_id;
+    let userID = req.query.user_id;
     console.log(userID);
 
     sqlManager.getUserNests(userID, async function(err, result) {
@@ -21,7 +89,7 @@ router.get('/userNests', async function(req, res) {
 
 // get items for specific nest
 router.get('/nestItems', async function(req, res) {
-    let nestID = req.params.nest_id;
+    let nestID = req.query.nest_id;
     console.log(nestID);
 
     sqlManager.getNestItems(nestID, async function(err, result) {
@@ -33,10 +101,61 @@ router.get('/nestItems', async function(req, res) {
     });
 });
 
+//Add nest item
+router.post('/addNestItem', upload.array('image',1), async function(req, res) {
+    var photo = "https://www.froben11.de/wp-content/uploads/2016/10/orionthemes-placeholder-image.png";
+    if(req.files && req.files.length > 0) {
+        photo = req.files[0].path;
+    }
+    nestItem = {
+        name: req.body.name,
+        description: req.body.description,
+        user_id: req.body.user_id,
+        nest_id: req.body.nest_id,
+        worth: req.body.worth,
+        photo: photo,
+        is_public: req.body.is_public
+    }
+
+    sqlManager.addNestItem(nestItem, function(err, result) {
+        if (err) {
+            res.status(500).json({status:'Failed', message: err.message});
+            return
+        }
+        res.status(200).json({status:'Success', message:'Nest Item Added Succesfully'});
+    });
+})
+
+
+//Edit nest item
+router.put('/editNestItem', upload.array('image',1), async function(req, res) {
+    
+    nestItem = {
+        id: req.body.id,
+        name: req.body.name,
+        description: req.body.description,
+        worth: req.body.worth,
+        is_public: req.body.is_public ?? 0,
+        favored: req.body.favored ?? 0,
+        nest_id: req.body.nest_id ?? -1
+    }
+    
+    if(req.files && req.files.length > 0) {
+        nesItem.photo = req.files[0].path;
+    }
+
+    sqlManager.editNestItem(nestItem, function(err, result) {
+        if (err) {
+            res.status(500).json({status:'Failed', message: err.message});
+            return
+        }
+        res.status(200).json({status:'Success', message:'Nest Item Edited Succesfully'});
+    });
+})
 
 // get specific nest item
 router.get('/nestItem', async function(req, res) {
-    let nestItemID = req.params.nest_item_id;
+    let nestItemID = req.query.nest_item_id;
     console.log(nestItemID);
 
     sqlManager.getNestItem(nestItemID, async function(err, result) {
--- backend/sql.js
+++ backend/sql.js
@@ -1,4 +1,5 @@
 const { O_NOFOLLOW } = require('constants');
+const { query } = require('express');
 const mysql = require('mysql');
 
 config = mysql.c
@@ -31,7 +32,7 @@ function registerUser(user, cb) {
 }
 
 function getUser(email, cb) {
-    connection.query("SELECT id,username,password,email,created_at,sortMode,onlyFavored FROM User u WHERE u.email = '" + email + "'",
+    connection.query("SELECT id,username,password,email,created_at,sort_mode,only_favored,is_asc,photo,phone_number FROM User u WHERE u.email = '" + email + "'",
         function (err, rows) {
             if (err) cb(err);
             else cb(undefined, rows);
@@ -46,7 +47,46 @@ function getUserofId(id, cb) {
         });
 }
 
-//Nest 
+//Add new Nest
+function addNest(nest, cb) {
+    connection.query("INSERT INTO Nest (name,description,favored,user_id,photo,created_at) VALUES('" + nest.name + "','" + nest.description + "',0," + nest.user_id + ",'" + nest.photo + "',NOW())", function (err, rows) {
+        if (err) cb(err);
+        else cb(undefined, rows);
+    });
+} 
+
+//Edit Nest
+function editNest(nest, cb) {
+    var query = "UPDATE Nest SET name = '" + nest.name + "', description = '" + nest.description + "' ,favored = " + nest.favored;
+    if (nest.photo !== undefined) {
+        query += ", photo ='" + nest.photo + "'";
+    }
+    query += "where id = " + nest.id;
+    console.log(query);
+
+    connection.query(query , function (err, rows) {
+        if (err) cb(err);
+        else cb(undefined, rows);
+    });
+} 
+
+//Delete Nest
+function deleteNest(nestID, cb) {
+    connection.query("DELETE n.*, ni.* FROM Nest n, NestItem ni WHERE ni.nest_id = n.id AND n.id =" + nestID , function (err, rows) {
+        if (err) cb(err);
+        else cb(undefined, rows);
+    });
+}
+
+//func update Nest Worth
+function updateNestWorth(id) {
+    var query = "UPDATE Nest n set n.total_worth = ( SELECT SUM(ni.worth) from NestItem ni WHERE ni.nest_id = "+ id +") WHERE n.id = " + id;
+    connection.query( query , function (err, rows) {
+        if (err) console.log(err);
+        else console.log("success fully update worth for nest_id"+id);
+    });
+}
+
 function getUserNests(id, cb) {
     connection.query("SELECT * FROM Nest n WHERE n.user_id = " + id,
         function (err, rows) {
@@ -71,6 +111,35 @@ function getNestItem(id, cb) {
             if (err) cb(err);
             else cb(undefined, rows);
         });
+}
+
+//Add NestItem
+function addNestItem(nestItem, cb) {
+    connection.query("INSERT INTO NestItem (name,description,favored,worth,user_id,nest_id,photo,created_at) VALUES('" + nestItem.name + "','" + nestItem.description + "',0," + nestItem.worth + "," + nestItem.user_id + "," + nestItem.nest_id + ",'" + nestItem.photo + "',NOW())", function (err, rows) {
+        if (err) cb(err);
+        else {
+            updateNestWorth(nestItem.nest_id)
+            cb(undefined, rows);
+        }
+    });
+} 
+
+//Edit Nest item
+function editNestItem(nestItem, cb) {
+    var query = "UPDATE NestItem SET name = '" + nestItem.name + "', description = '" + nestItem.description + "',favored = " + nestItem.favored + ",worth = " + nestItem.worth + ", is_public = " + nestItem.is_public;
+    if (nestItem.photo !== undefined) {
+        query += ", photo = '" + nestItem.photo  + "'";
+    }
+    query += " where id = " + nestItem.id;
+    console.log(query);
+
+    connection.query(query, function (err, rows) {
+        if (err) cb(err);
+        else {
+            updateNestWorth(nestItem.nest_id)
+            cb(undefined, rows);
+        }
+    });
 }
 
 //Feeds
@@ -116,9 +185,14 @@ module.exports = {
     registerUser: registerUser,
     getUser: getUser,
     getUserofId: getUserofId,
+    addNest: addNest,
+    editNest: editNest,
+    deleteNest: deleteNest,
     getUserNests: getUserNests,
     getNestItems: getNestItems,
     getNestItem: getNestItem,
+    addNestItem: addNestItem,
+    editNestItem: editNestItem,
     getFeeds: getFeeds,
     getFeedUserNests: getFeedUserNests,
     getFeedUserNestItems: getFeedUserNestItems,
