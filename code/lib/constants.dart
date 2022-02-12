cf935255915578858e2b3e7c8b1e6baa86df1698
Alexander Wiegel <alexanderwiegel17@gmail.com>
2022-02-12 16:23:35 +0100

    Remove parameter types from colored print functions to enable printing more than just Strings

--- code/lib/constants.dart
+++ code/lib/constants.dart
@@ -8,13 +8,13 @@ const MaterialColor accentColor = Colors.amber;
 
 // Colored print() functions using ANSI escape codes for better debugging
 
-void printError(String text) => printColored(31, text); // red
+void printError(text) => printColored(31, text); // red
 
-void printSuccess(String text) => printColored(32, text); // green
+void printSuccess(text) => printColored(32, text); // green
 
-void printWarning(String text) => printColored(33, text); // yellow
+void printWarning(text) => printColored(33, text); // yellow
 
-void printInfo(String text) => printColored(34, text); // blue
+void printInfo(text) => printColored(34, text); // blue
 
 /*
 void printXXX(String text) => printColored(35, text); // magenta
@@ -22,6 +22,6 @@ void printYYY(String text) => printColored(36, text); // cyan
 void printZZZ(String text) => printColored(37, text); // white
  */
 
-void printColored(int value, String text) {
+void printColored(value, text) {
   print("\x1B[${value.toString()}m$text\x1B[0m");
 }
