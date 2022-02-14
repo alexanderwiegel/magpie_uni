199019ebc86f872a6d358fa259b0d299c9327a2b
Alexander Wiegel <alexanderwiegel17@gmail.com>
2022-02-14 23:05:02 +0100

    Don't set a default value for the currentUserId at all

--- code/lib/network/user_api_manager.dart
+++ code/lib/network/user_api_manager.dart
@@ -3,14 +3,11 @@ import 'package:magpie_uni/model/feed.user.profile.model.dart';
 class UserAPIManager {
   static final UserAPIManager _shared = UserAPIManager._internal();
 
-  factory UserAPIManager() {
-    return _shared;
-  }
+  factory UserAPIManager() => _shared;
 
   static late FeedUserProfileResponse currentUserProfile;
 
-  // TODO: make sure this is set after login
-  static late int currentUserId = -1;
+  static late int currentUserId;
   static String token =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjo1LCJpYXQiOjE2NDQ0MTMyOTgsImV4cCI6MTY0NDQxNjg5OH0.jYQ8cRpmCjaKwELaYTne2tFdz2V6d7yYBB2VGN27QIk";
 
